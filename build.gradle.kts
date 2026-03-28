a51e3501eab4d6489170922cd433bed40e7aa9aa
Sayaem Arafat <sayaemarafat360@gmail.com>
2026-03-29 04:03:02 +0600

    hgfdf

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     id("com.android.library") version libs.versions.agp apply false
     id("org.jetbrains.kotlin.android") version libs.versions.kotlin apply false
     id("org.jetbrains.kotlin.kapt") version libs.versions.kotlin apply false
-    id("com.google.dagger.hilt.android") version libs.versions.hilt apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
     id("com.google.devtools.ksp") version libs.versions.ksp apply false
 }
